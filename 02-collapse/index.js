c33a5e12081cbbb85d75a0c238857209205b7b19
Nikita Dymont <gg.joke55@gmail.com>
2022-09-01 22:32:52 +0300

    implement some features and fixes

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,28 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+let contentIsVisible = true,
+  titleHide = document.querySelector(".collapsible__action--hidden"),
+  titleHideVisible = document.querySelector(".collapsible__action--visible"),
+  content = document.querySelector(".collapsible__content"),
+  contentHide = [{ opacity: 1 }, { opacity: 0 }],
+  contentShow = [{ opacity: 0 }, { opacity: 1 }];
+
+document.querySelector(".collapsible__action--hidden").style.display = "none";
+
+document.querySelector(".collapsible__button").onclick = () => {
+  if (contentIsVisible) {
+    titleHideVisible.style.display = "none";
+    titleHide.style.display = "block";
+
+    content.animate(contentHide, 500);
+
+    setTimeout(() => {
+      content.style.display = "none";
+    }, 490);
+  } else {
+    titleHideVisible.style.display = "block";
+    titleHide.style.display = "none";
+    content.style.display = "block";
+
+    content.animate(contentShow, 500);
+  }
+  contentIsVisible = !contentIsVisible;
+};
